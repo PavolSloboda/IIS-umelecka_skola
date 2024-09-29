52fa780aa6c4524ba6f07a764b3789c4c15639d7
PavolSloboda <pavol.sloboda02@gmail.com>
2024-09-29 12:20:00 +0200

    added an initial service(not finished)

---
+++ umelecka_skola/app/Core/Service.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+use Nette;
+use Nette\Database\Explorer;
+
+//TODO
+//figure out what this should extend
+
+//Main Service, other services will be derived from it
+final class Service extends Nette\Application\Router
+{
+	private Explorer $database;
+
+	public function __construct(Explorer $database)
+	{
+		$this->database = $database;
+	}
+}
