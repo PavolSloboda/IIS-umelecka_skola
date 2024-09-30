9813974ae044028b2868d4adfd58008f4b121cfa
PavolSloboda <pavol.sloboda02@gmail.com>
2024-09-30 16:18:59 +0200

    added a basic device service containing the showalldevices method

---
+++ umelecka_skola/app/Core/DevicesService.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+use Nette\Database\Explorer;
+
+final class DevicesService
+{
+	private Explorer $database;
+
+	public function __construct(Explorer $database)
+	{
+		$this->database = $database;
+	}
+
+	/*
+	* @return Nette\Database\table\ActiveRow[]
+	*/
+	public function showAllDevices() : array
+	{
+		$result = $this->database->table('devices')->fetchAll();
+		return $result;
+	}
+}
