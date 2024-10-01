e70a5e7e471d8934a99eaa274a79a833469ad22e
PavolSloboda <pavol.sloboda02@gmail.com>
2024-10-01 12:42:25 +0200

    implemented the functionality for the log in screen

--- umelecka_skola/app/Core/LoginService.php
+++ umelecka_skola/app/Core/LoginService.php
@@ -4,10 +4,44 @@ declare(strict_types=1);
 
 namespace App\Core;
 
-final class LoginService
-{
-	public function validateLogin() : void
-	{
+use Nette;
+use Nette\Security\SimpleIdentity;
+use Nette\Security\Passwords;
+use Nette\Database\Explorer;
 
+final class LoginService implements Nette\Security\Authenticator
+{
+
+	private $database;
+	private $passwords;
+
+	public function __construct(Explorer $database, Passwords $passwords)
+	{
+		$this->database = $database;
+		$this->passwords = $passwords;
+	}
+
+	public function authenticate(string $email, string $password) : SimpleIdentity
+	{
+		//getting a user from the database
+		$user =  $this->database->table('users')->where('email', $email)->fetch();
+
+		//checking whether the specified user exists
+		if(!$user)
+		{
+			throw new Nette\Security\AuthenticationException('User with the provided email does not exist, please sign up instead');
+		}
+		//checking whether the password is correct
+		elseif (!$this->passwords->verify($password, $user->password))
+		{
+			throw new Nette\Security\AuthenticationException('Invalid password, please try again');
+		}
+
+		return new SimpleIdentity($user->user_id, ['email' => $user->email]);
+	}
+
+	public function signUp(string $email, string $password): void
+	{
+		$this->database->table('users')->insert(['email' => $email, 'password' => $this->passwords->hash($password),]);
 	}
 }
--- umelecka_skola/app/UI/Login/LoginPresenter.php
+++ umelecka_skola/app/UI/Login/LoginPresenter.php
@@ -6,26 +6,42 @@ namespace App\UI\Login;
 
 use Nette\Application\UI\Form;
 use Nette;
+use App\Core\LoginService;
 
 final class LoginPresenter extends Nette\Application\UI\Presenter
 {
+
+	private $loginService;
+
+	public function __construct(LoginService $loginService)
+	{
+		//initialises the login service
+		$this->loginService = $loginService;
+		//$this->loginService->signUp('test@test.com', 'test');
+	}
 
 	public function createComponentLoginForm() : Form
 	{
 		$form = new Form;
 
-		$form->addText('email', 'Email:');
-		$form->addText('password', 'Password:');
+		$form->addEmail('email', 'Email:')->setRequired('Plase enter an email');
+		$form->addPassword('password', 'Password:')->setRequired('Plase enter your password');
 		$form->addSubmit('login', 'Log in');
-		$form->addSubmit('signup', 'Sign up');
 		
+		$form->onSuccess[] = [$this, 'validateLogin']; 
 		return $form;
 	}
 
-	public function validateLogin() : void
+	public function validateLogin(Form $form, \stdClass $data) : void
 	{
-
+		try
+		{
+			$this->getUser()->setAuthenticator($this->loginService)->login($data->email, $data->password);
+			$this->redirect('Devices:devices');
+		}
+		catch (Nette\Security\AuthenticationException $e)
+		{
+			$form->addError('Invalid email or password');
+		}
 	}
-
-
 }
