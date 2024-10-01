4f2bcaba8f409be07173e963c9bd46345fec1bfb
PavolSloboda <pavol.sloboda02@gmail.com>
2024-10-01 09:46:42 +0200

    created a basic page listing all devices and set is as the default page

---
+++ umelecka_skola/app/UI/Devices/DevicesPresenter.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Devices;
+
+use App\Core\DevicesService;
+use Nette;
+
+final class DevicesPresenter extends Nette\Application\UI\Presenter
+{
+	private $devices; 
+
+	public function __construct(DevicesService $devices)
+	{
+		$this->devices = $devices;
+	}
+
+	public function renderDevices() : void
+	{
+		$this->template->result = $this->devices->showAllDevices();
+	}
+}
