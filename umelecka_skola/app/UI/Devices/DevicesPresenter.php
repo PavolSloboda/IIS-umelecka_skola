452293e860b5c6c3812092b19d90ac2d70b3bf5f
TerezaFortelna <tereza.for@seznam.cz>
2024-10-02 13:36:59 +0200

    implementovany logout

--- umelecka_skola/app/UI/Devices/DevicesPresenter.php
+++ umelecka_skola/app/UI/Devices/DevicesPresenter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\UI\Devices;
 
 use App\Core\DevicesService;
+use Nette\Application\UI\Form;
 use Nette;
 
 final class DevicesPresenter extends Nette\Application\UI\Presenter
@@ -19,5 +20,17 @@ final class DevicesPresenter extends Nette\Application\UI\Presenter
 	public function renderDevices() : void
 	{
 		$this->template->result = $this->devices->showAllAvailableDevices();
+	}
+
+	public function createComponentLogoutForm() : Form
+	{
+		$form = new Form;
+
+		$form->addButton('logout', 'Log out')->setHtmlAttribute('onclick', 'window.location.href="'.$this->link('loginClicked!').'"');
+		return $form;
+	}
+	public function handleLoginClicked() : void
+	{
+		$this->redirect('Login:login');
 	}
 }
