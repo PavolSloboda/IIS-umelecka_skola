b28905e095661d744e418e132cab23f03d79e627
PavolSloboda <pavol.sloboda02@gmail.com>
2024-10-03 13:58:34 +0200

    added an insert atelier method to the service

--- umelecka_skola/app/Core/AtelierService.php
+++ umelecka_skola/app/Core/AtelierService.php
@@ -14,4 +14,22 @@ final class AtelierService
 	{
 		$this->database = $database;
 	}
+
+	public function createAtelier(string $name, string $admin_email) : void
+	{
+		$admin = $this->database->table('users')->where('email', $admin_email)->fetch();
+
+		if(!$admin)
+		{
+			throw new \Exception("User with email: {$admin_email} not found, please make usre you've entered the correct email");
+		}
+		//TODO
+		//add the necessary roles
+	//	if($admin->role != xyz)
+	//	{
+	//		throw new \Exception("User with email: {$admin_email} does not have the required roles to aminister an atelier");
+	//	}
+
+		$this->database->table('ateliers')->insert(['name' => $name, 'admin_id' => $admin->id,]);
+	}
 }
