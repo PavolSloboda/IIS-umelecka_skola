d2488a3fcb700b2361479e63ebafab20f9f4931a
TerezaFortelna <tereza.for@seznam.cz>
2024-10-08 13:27:49 +0200

    pridavani a zobrazeni rezervaci

--- umelecka_skola/app/Core/DevicesService.php
+++ umelecka_skola/app/Core/DevicesService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Core;
 
 use Nette\Database\Explorer;
+use Nette\Utils\DateTime;
 
 final class DevicesService
 {
@@ -17,19 +18,47 @@ final class DevicesService
 
 	/*
 	* @return Nette\Database\table\ActiveRow[]
-	*/
+	*///vypis vsech dostupnych zarizeni
 	public function showAllAvailableDevices() : array
 	{
-		$result = $this->database->table('devices')->fetchAll();
+		$result = $this->database->table('devices')->where('loan',false)->fetchAll();
+		return $result;
+	}
 
-		//$result = $this->database->table('devices')->where('reserved', false)->where('borrow',false)->fetchAll();
-		return $result;
-	}
-	public function showAllDevices() : array
+	public function showAllAvailableLoans(int $userId) : array
 	{
-		$result = $this->database->table('devices')->fetchAll();
+		$result = $this->database->table('loan')->where('user_id',$userId)->fetchAll();
 		return $result;
 	}
+
+	//vypis jmen vsech dostupnych zarizeni
+	public function getAvailableDevices(): array
+    {
+        $devices = $this->database->table('devices')->where('loan', FALSE);
+
+		$deviceOptions = [];
+        foreach ($devices as $device) {
+            $deviceOptions[$device->device_id] = $device->name;
+        }
+
+        return $deviceOptions;
+    }
+
+	//pujceni zarizeni, todo maximalni doba vypujcky, zarizeni ktera nejdou vypujcit nebo omezit na atelier, spravovani zarizeni majitelem a spravuje vraceni a pujceni
+    public function borrowDevice(int $userId, int $deviceId, string $loanStart, string $loanEnd): void
+    {
+        $device = $this->database->table('devices')->get($deviceId);
+
+        if ($device && !$device->loan) {
+            $this->database->table('loan')->insert(['user_id' => $userId,'device_id' => $deviceId,'loan_start' => $loanStart,'loan_end' => $loanEnd,'status' => 'reservation',]);
+            $device->update(['loan' => TRUE,]);
+        }
+    }
+
+	public function getDeviceById(int $deviceId)
+    {
+        return $this->database->table('devices')->get($deviceId);
+    }
 }
 //maximalni doba vypujcky
 //vyucujici si vypujcuje jen z vlastnich atelieru
--- umelecka_skola/app/UI/Devices/DevicesPresenter.php
+++ umelecka_skola/app/UI/Devices/DevicesPresenter.php
@@ -5,16 +5,21 @@ declare(strict_types=1);
 namespace App\UI\Devices;
 
 use App\Core\DevicesService;
-use Nette\Application\UI\Form;
 use Nette;
+use Nette\Application\UI\Form;
+
 
 final class DevicesPresenter extends Nette\Application\UI\Presenter
 {
 	private $devices; 
+	private DevicesService $DevicesService;
+	private $loan;
 
-	public function __construct(DevicesService $devices)
+	public function __construct(DevicesService $devices,DevicesService $DevicesService,DevicesService $loan)
 	{
 		$this->devices = $devices;
+		$this->DevicesService = $DevicesService;
+		$this->loan = $loan;
 	}
 
 	protected function startup() : void
@@ -26,9 +31,86 @@ final class DevicesPresenter extends Nette\Application\UI\Presenter
 		}
 	}
 
+	//vypis vsech dostupnych zarizeni
 	public function renderDevices() : void
 	{
-		$this->template->result = $this->devices->showAllAvailableDevices();
+		$this->template->devices = $this->devices->showAllAvailableDevices();
+		$this->template->loans = $this->devices->showAllAvailableLoans($this->getUser()->getId());
+
+	}
+	
+
+	//formular na pujceni zarizeni
+	public function createComponentAddDeviceLoanForm() : Form
+	{
+		$form = new Form;
+
+		$form->addSelect('device_id', 'Select Device:', $this->DevicesService->getAvailableDevices())->setPrompt('Choose a device')->setRequired('Please select a device.');
+
+		// Zadání začátku výpůjčky (datum a čas)
+		$form->addDateTime('loan_start', 'Start Date and Time:')->setFormat('Y-m-d H:i:s')
+        ->setRequired('Please enter the start date and time.');
+	
+		$form->addDateTime('loan_end', 'End Date and Time:')->setFormat('Y-m-d H:i:s')
+        ->setRequired('Please enter the end date and time.');
+		
+		$form->addSubmit('submit', 'Borrow Device');
+
+		$form->onValidate[] = [$this, 'validateAddDeviceLoanForm'];
+
+		$form->onSuccess[] = [$this, 'processAddDeviceLoanForm'];
+
+		return $form;
+		
+	}
+
+	public function validateAddDeviceLoanForm(Form $form): void
+	{
+		date_default_timezone_set('Europe/Prague');
+		$loanStart = new \DateTime($form->getValues()->loan_start);
+		$loanEnd = new \DateTime($form->getValues()->loan_end);
+
+		$interval = $loanStart->diff($loanEnd);
+		$maxLoanDuration = $this->DevicesService->getDeviceById($form->getValues()->device_id)->max_loan_duration;
+
+		if ($interval->days > $maxLoanDuration) {
+			$form->addError('The loan duration cannot exceed ' . $maxLoanDuration . ' days.');
+		}
+
+		if ($loanEnd < $loanStart) {
+			$form->addError('The end date must be after the start date.');
+		}
+
+		$today = new \DateTime();
+		$formattoday = $today->format('d-m-Y H:i');
+		if ($today > $loanStart) {
+			$form->addError('The earliest possible reservation start date is ' . $formattoday . '.');
+		}
+		
+	}
+
+	public function processAddDeviceLoanForm(Form $form, \stdClass $values): void
+	{
+		if ($form->hasErrors()) {
+			return; 
+		}
+
+		$userId = $this->getUser()->getId();
+		$deviceId = $values->device_id;
+
+		// Kombinace data a času do jednoho řetězce pro databázi
+		$loanStart = $values->loan_start;
+		$loanEnd = $values->loan_end;
+
+		try {
+			$this->DevicesService->borrowDevice($userId, $deviceId, $loanStart, $loanEnd);
+			$this->flashMessage('Device has been successfully borrowed.', 'success');
+			
+			$this->redirect('this');
+		}catch(Nette\Security\AuthenticationException $e)
+		{
+			$form->addError('An error occured');
+		}
 	}
 
 }
