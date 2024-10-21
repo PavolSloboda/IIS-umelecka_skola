17f8c3adf2e853c4de982f7cdb45f77cb2b7a4cc
PavolSloboda <pavol.sloboda02@gmail.com>
2024-10-21 16:02:58 +0200

    the main page is main

--- umelecka_skola/app/Core/RouterFactory.php
+++ umelecka_skola/app/Core/RouterFactory.php
@@ -15,7 +15,7 @@ final class RouterFactory
 	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
-		$router->addRoute('<presenter>/<action>[/<id>]', 'Login:login');
+		$router->addRoute('<presenter>/<action>[/<id>]', 'MainPage:mainpage');
 		return $router;
 	}
 }
