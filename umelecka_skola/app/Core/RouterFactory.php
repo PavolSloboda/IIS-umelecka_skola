24268a57669aba33a8e7a98170aebbe6189cc5c8
David <kkajkocze@gmail.com>
2024-11-20 18:23:47 +0100

    some small improvement

--- umelecka_skola/app/Core/RouterFactory.php
+++ umelecka_skola/app/Core/RouterFactory.php
@@ -18,7 +18,7 @@ final class RouterFactory
 		$router->addRoute('<presenter>/<action>[/<id>]', 'MainPage:mainpage');
 
 		$router->addRoute('/myProfile', 'MyProfile:myProfile');    // Zobrazení profilu
-		$router->addRoute('/myProfile/edit', 'MyProfile:edit');     // Úprava profilu
+		$router->addRoute('/myProfile/editProfile', 'MyProfile:editProfile');     // Úprava profilu
 		$router->addRoute('/myProfile/passwordChange', 'MyProfile:passwordChange');  // Změna hesla
 		$router->addRoute('/myProfile/currentLoans', 'MyProfile:currentLoans');  // Aktuální výpůjčky
 		$router->addRoute('/myProfile/pastLoans', 'MyProfile:pastLoans');  // Minulé výpůjčky
