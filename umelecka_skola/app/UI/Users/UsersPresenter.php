b47694b3111766d77f3978b9fb168e221aa98c42
David <kkajkocze@gmail.com>
2024-11-22 17:31:34 +0100

    hope user is already fine

--- umelecka_skola/app/UI/Users/UsersPresenter.php
+++ umelecka_skola/app/UI/Users/UsersPresenter.php
@@ -50,7 +50,13 @@ final class UsersPresenter extends Nette\Application\UI\Presenter
 
     // Načtení seznamu rolí z RolesService
     $roles = $this->roles->getRoles();
+
+    // Přidání možnosti "Žádná role"
+    $roles[''] = 'No role'; // Případ pro "Žádnou roli" (prázdný klíč bude znamenat žádnou roli)
+
     $form->addSelect('role', 'Role:', $roles)
+        ->setRequired('Please select a role.');
+    $form->addSelect('role2', 'Role:', $roles)
         ->setRequired('Please select a role.');
 
     $form->addSubmit('submit', 'Save');
@@ -77,11 +83,22 @@ final class UsersPresenter extends Nette\Application\UI\Presenter
 
     // Aktualizace role
     $roleId = $values->role;
-    if ($roleId == 0) {  // "0" znamená "Registrovaný uživatel" (žádná role)
+    $roleId2 = $values->role2;
+    if(($roleId == 2 && $roleId2 == 3) || ($roleId == 3 && $roleId2 == 2)){
+        $this->usersService->updateUserRoletwo($userId, $roleId, $roleId2);
+    } else if(($roleId == 0 && $roleId2 != '') || ($roleId == 4 && $roleId2 != '') || ($roleId == 2 && ($roleId2 != 3 || $roleId2 != '')) || ($roleId == 3 && ($roleId2 != 2 || $roleId2 != ''))){
+        $form->addError('Wrong role input.');
+        return;
+    }else if(($roleId2 == 0 && $roleId != '') || ($roleId2 == 4 && $roleId != '') || ($roleId2 == 2 && ($roleId != 3 || $roleId != '')) || ($roleId2 == 3 && ($roleId != 2 || $roleId != ''))){
+        $form->addError('Wrong role input.');
+        return;
+    }
+    else if ($roleId == 0 || $roleId2 == 0) {  // "0" znamená "Registrovaný uživatel" (žádná role)
         $this->usersService->removeUserRole($userId);
-    }else{
-	bdump($roleId);
+    }else if($roleId2 == ''){
     $this->usersService->updateUserRole($userId, $roleId);
+    }else{
+        $this->usersService->updateUserRole($userId, $roleId2);
     }
     $this->flashMessage('User information updated successfully.', 'success');
     $this->redirect('users');
@@ -106,7 +123,12 @@ final class UsersPresenter extends Nette\Application\UI\Presenter
 
 	public function actionDelete(int $userId): void
 	{
+        try{
 		$this->usersService->deleteUser($userId);
+        }
+        catch(Exception $e) {
+            echo 'Message: ' .$e->getMessage();
+        }
         $this->usersService->removeUserRole($userId);
 		$this->flashMessage('User deleted successfully.', 'success');
 		$this->redirect('users');
